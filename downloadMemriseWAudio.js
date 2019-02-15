6e71de2adb0e48518ee16ee06f6b1d52de16096c
ktrain2 <44568383+ktrain2@users.noreply.github.com>
2019-02-15 21:45:06 +1100

    Update downloadMemriseWAudio.js

--- downloadMemriseWAudio.js
+++ downloadMemriseWAudio.js
@@ -3,7 +3,15 @@
 // Original source: https://gist.github.com/raineorshine/68fab3b4b96f54b808a858217b83fe94
 
 // See the readme for usage instructions
+//static includeAudio = 0;
+
+
+//var CHINESE = 1000
+//var ITALIAN = 1001
+
 (() => {
+	//Set whether Audio files should be downloaded. NOTE: It is highly recommended you temporarily setup auto-download, because hundreds of files are required to download.
+	var includeAudio = 1;
 	
 	function messySleep(miliseconds) {
 		// Creates a delay, while preventing other things happening at the same time
@@ -11,9 +19,39 @@
 		while (currentTime + miliseconds >= new Date().getTime()) {
 		}
 	}
+	
+	function downloadAudio(filename) {
+		if(filename==null){
+			return -1;
+		}
+		fetch(`https://static.memrise.com/uploads/items/audios/${filename}`, { credentials: 'same-origin' })
+		.then((resp) => resp.arrayBuffer())
+		.then( data => {
+			console.log(data);
+			var blob = new Blob([data], {type: 'mp3'});
+			var elem = window.document.createElement('a');
+			elem.href = window.URL.createObjectURL(blob);
+			console.log(elem.href);
+			elem.download = `memrise_${filename}`;
+			document.body.appendChild(elem);
+			elem.click();
+			
+			//elem.click();
+			
+			//elem.click();
+			//elem.click();
+			//console.log("HELLO");
+			document.body.removeChild(elem);
+			window.document.delete(Element
+			//window.URL.revokeObjectURL(url
+			messySleep(300);
+			return 1;
+		})
+	}
+	
 	function getWords(courseId, level) {		
 		const url = `https://www.memrise.com/ajax/session/?course_id=${courseId}&level_index=${level}&session_slug=preview`
-		console.log(url)
+		console.log(url);
 		return fetch(url, { credentials: 'same-origin' })
 			// parse response
 			.then(res => {
@@ -25,14 +63,17 @@
 						messySleep(100)
 						return Object.keys(data.learnables).map(key => (
 							  
-						{
+						((includeAudio==1)?({
 							original: data.learnables[key].item.value,
 							translation: data.learnables[key].definition.value,
-							hanzi: data.screen_template_map[data.learnables[key].learnable_id].presentation[0].visible_info[(data.screen_template_map[data.learnables[key].learnable_id].presentation[0].visible_info[0].label=="Character")?0:1].value,							
+							//hanzi: data.screen_template_map[data.learnables[key].learnable_id].presentation[0].visible_info[(data.screen_template_map[data.learnables[key].learnable_id].presentation[0].visible_info[0].label=="Character")?0:1].value,							
 							
 							audio_reference: (data.screen_template_map[data.learnables[key].learnable_id].presentation[0].audio.value[0]==null)?null:data.screen_template_map[data.learnables[key].learnable_id].presentation[0].audio.value[0].normal.slice(48)
 
-						}))
+						}):({
+							original: data.learnables[key].item.value,
+							translation: data.learnables[key].definition.value
+						}))))
 						
 					})
 					.then(words => {
@@ -48,38 +89,20 @@
 		})
 	}
   
-	function downloadAudio(filename) {
-		if(filename==null){
-			return -1;
-		}
-		fetch(`https://static.memrise.com/uploads/items/audios/${filename}`, { credentials: 'same-origin' })
-		.then((resp) => resp.arrayBuffer())
-		.then( data => {
-			console.log(data)
-			var blob = new Blob([data], {type: 'mp3'})
-			var elem = window.document.createElement('a');
-			elem.href = window.URL.createObjectURL(blob);
-			elem.download = `memrise_${filename}`;
-			document.body.appendChild(elem);
-			elem.click();
-			document.body.removeChild(elem);
-			//window.URL.revokeObjectURL(url
-			messySleep(300)
-		})
-	}
 	var fora;
 	// fetch
-	const start = 1
-	const courseId = location.href.slice(30).match(/\d+/)[0] 
+	const start = 18;
+	const courseId = location.href.slice(30).match(/\d+/)[0];
 
 	getWords(courseId, start)
 	// format as csv
 	.then(words => {
 		//console.log(word.audio_reference) //Check if the base url works in this case
-		words.map(word => downloadAudio(word.audio_reference))//Enable to download Audio
-		console.log(words.length + ' words')
-		return words.map(word => word.translation + '\t' + word.original + '\t' + word.hanzi + '\t[memrise_' + word.audio_reference + ']\n').join('') //for 4 data things
-		//return words.map(word => word.translation + '\t' + word.original + '\t[memrise_' + word.audio_reference + ']\n').join('') //for 3 data things
+		if (includeAudio==1){words.map(word => downloadAudio(word.audio_reference))};//Enable to download Audio
+		console.log(words.length + ' words');
+		//return words.map(word => word.translation + '\t' + word.original + '\t' + word.hanzi + '\t[memrise_' + word.audio_reference + ']\n').join('') //for 4 data things
+		return ((includeAudio==1)?words.map(word => word.translation + '\t' + word.original + '\t[memrise_' + word.audio_reference + ']\n').join(''): //for 3 data things
+		words.map(word => word.translation + '\t' + word.original + '\n').join('')) //for 3 data things
 	})
 	.then(console.log)
 })()
