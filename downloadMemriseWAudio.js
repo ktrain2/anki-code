5e43885ed476dcf97bc24ed8cf7486f28a4d9759
ktrain2 <44568383+ktrain2@users.noreply.github.com>
2019-12-07 15:34:15 +1100

    Got working for Brazilian Portuguese

--- downloadMemriseWAudio.js
+++ downloadMemriseWAudio.js
@@ -28,9 +28,8 @@
 						{
 							original: data.learnables[key].item.value,
 							translation: data.learnables[key].definition.value,
-							hanzi: data.screen_template_map[data.learnables[key].learnable_id].presentation[0].visible_info[(data.screen_template_map[data.learnables[key].learnable_id].presentation[0].visible_info[0].label=="Character")?0:1].value,							
 							
-							audio_reference: (data.screen_template_map[data.learnables[key].learnable_id].presentation[0].audio.value[0]==null)?null:data.screen_template_map[data.learnables[key].learnable_id].presentation[0].audio.value[0].normal.slice(48)
+							audio_reference: (data.screen_template_map[data.learnables[key].learnable_id].presentation[0].audio.value[0]==null)?null:data.screen_template_map[data.learnables[key].learnable_id].presentation[0].audio.value[0].normal
 
 						}))
 						
@@ -48,17 +47,18 @@
 		})
 	}
   
-	function downloadAudio(filename) {
-		if(filename==null){
+	function downloadAudio(filepath) {
+		if(filepath==null){
 			return -1;
 		}
-		fetch(`https://static.memrise.com/uploads/items/audios/${filename}`, { credentials: 'same-origin' })
+		fetch(`${filepath}`, { credentials: 'same-origin' })
 		.then((resp) => resp.arrayBuffer())
 		.then( data => {
 			console.log(data)
 			var blob = new Blob([data], {type: 'mp3'})
 			var elem = window.document.createElement('a');
 			elem.href = window.URL.createObjectURL(blob);
+			var filename = filepath.replace('/','_');
 			elem.download = `memrise_${filename}`;
 			document.body.appendChild(elem);
 			elem.click();
@@ -78,7 +78,7 @@
 		//console.log(word.audio_reference) //Check if the base url works in this case
 		words.map(word => downloadAudio(word.audio_reference))//Enable to download Audio
 		console.log(words.length + ' words')
-		return words.map(word => word.translation + '\t' + word.original + '\t' + word.hanzi + '\t[memrise_' + word.audio_reference + ']\n').join('') //for 4 data things
+		return words.map(word => word.translation + '\t' + word.original + '\t[memrise_' + word.audio_reference + ']\n').join('') //for 4 data things
 		//return words.map(word => word.translation + '\t' + word.original + '\t[memrise_' + word.audio_reference + ']\n').join('') //for 3 data things
 	})
 	.then(console.log)
